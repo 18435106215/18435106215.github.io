b47de9a95b5511cfcd259b5dcc27cfc84fa1c983
18435106215 <757293200@qq.com>
2017-11-01 16:30:40 +0800

    1

---
+++ 添仓网2.0/js/login.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/10/26.
+ */
+//前台登录验证
+$(function () {
+    var account=$('input[type=text]');
+    var password=$('input[type=password]');
+    var submit=$('input[type=submit]');
+    // console.log(submit)
+
+    account.add(password).blur(function () {
+        if(account.val()){
+            submit.removeClass('active')
+        }else {
+
+        }
+    })
+})
