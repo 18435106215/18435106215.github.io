2b96fa8f8410306fba9dafc74ff4ee481f49f64a
18435106215 <757293200@qq.com>
2017-11-04 12:25:14 +0800

    11

---
+++ 添仓网2.0/js/fapiaoxinxi.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/11/4.
+ */
+$(function () {
+    $('section .fapiao div').click(function () {
+        $('section .fapiao ul.fapiao_list').slideToggle(500)
+    })
+})
